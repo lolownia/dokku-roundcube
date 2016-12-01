fed2109359912e7dc59822163eca42243a64f9f7
Marcin Koziej <marcin@cahoots.pl>
2016-12-01 16:01:38 +0100

    DATABASE_URL

--- config.inc.php
+++ config.inc.php
@@ -10,7 +10,7 @@
 // Currently supported db_providers: mysql, pgsql, sqlite, mssql or sqlsrv
 // For examples see http://pear.php.net/manual/en/package.database.mdb2.intro-dsn.php
 // NOTE: for SQLite use absolute path: 'sqlite:////full/path/to/sqlite.db?mode=0646'
-$config['db_dsnw'] = getenv('DB_DSNW');
+$config['db_dsnw'] = getenv('DATABASE_URL
 
 // ----------------------------------
 // IMAP
